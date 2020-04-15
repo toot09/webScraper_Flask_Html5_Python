278bc80c83596b690d439dcce42a71ff39fbae46
toot09 <03toot28@gmail.com>
2020-04-15 04:53:57 +0000

    plus Scraping into Webpage

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from scraper import get_jobs
 app = Flask("SuperScraper")
 
 # @ : decorator
 @app.route("/")
 def home():
-  return "hello!"
+  # render_template get html template
+  return render_template("main.html")
 
-@app.route("/contact")
-def contact():
-  return "Contact me!"
+@app.route("/search")
+def search():
+  #request.args
+  word = request.args.get("word")
+  # 입력값이 None(null)이면 소문자처리, 입력값없으면 홈으로(/) redirect
+  if word: 
+    word = word.lower()
+    jobs = get_jobs(word)
+    print(jobs)
+  else:
+    return redirect("/")
+  return render_template("search.html",word=word)
+  #return f"Do you want find \"{word}\" jobs?"
+
+# Should use param(username) in instance
+#@app.route("/<username>")
+#def contact(username):
+#  return f"Hello! {username}. How are you doing?"
 
 app.run(host="0.0.0.0")
---
+++ scraper.py
@@ -0,0 +1,50 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_last_page(URL):
+    result = requests.get(URL)
+    soup = BeautifulSoup(result.text, "html.parser")
+    pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
+    #print(pages[-2].find("span").string)
+    last_page = pages[-2].get_text(strip=True)
+    return int(last_page)
+
+
+def extract_job(html):
+    title = html.find("div", {"class": "fl1"}).find("h2").find("a")["title"]
+    #h3 태그에 회사명과 지역이 span으로 두개가 동시에 있기 때문에 find_all
+    #recursive=False는 지정한 그 depth의 span만 가져온다.(span안에 span은 안가져옴. 여기는 해당없음.)
+    company, location = html.find("div", {
+        "class": "fl1"
+    }).find("h3").find_all(
+        "span", recursive=False)
+    company = company.get_text(strip=True)
+    location = location.get_text(strip=True)
+    job_id = html["data-jobid"]
+    return {
+        "title": title,
+        "company": company,
+        "location": location,
+        "link": f"https://stackoverflow.com/jobs/{job_id}"
+    }
+
+
+def extract_jobs(last_page,URL):
+    jobs = []
+    for page in range(last_page):
+        print(f"Scraping SO page : {page}")
+        result = requests.get(f"{URL}&pg={page+1}")
+        soup = BeautifulSoup(result.text, "html.parser")
+        results = soup.find_all("div", {"class": "-job"})
+        #results = soup.find_all("div",{"class":"grid"})
+        for result in results:
+            job = extract_job(result)
+            jobs.append(job)
+    return jobs
+
+
+def get_jobs(word):
+    URL = f"https://stackoverflow.com/jobs?q={word}"
+    last_page = get_last_page(URL)
+    jobs = extract_jobs(last_page,URL)
+    return jobs
